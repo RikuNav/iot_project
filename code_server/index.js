b2d1255a8cc982d9a85bd834822f7c1a806b931d
Peblo <a01275676@tec.mx>
2022-11-29 10:45:35 -0600

    server done

--- code_server/index.js
+++ code_server/index.js
@@ -3,6 +3,9 @@ const mysql = require('mysql');
 const { readFile } = require('fs');
 const { networkInterfaces } = require('os');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 // Declaraciones de ayuda para leer datos enviados como JSON y texto en http
 app.use(express.json());
@@ -16,19 +19,13 @@ app.use(express.static('../webPage'));
 const db = mysql.createConnection({
     host: 'localhost',            // Dónde está hosteada
     user: 'root',                 // Nombre de usuario
-    password: '',             // Contraseña de usuario
-    database: 'Iot'               // Nombre de la base de datos
+    password: 'root',             // Contraseña de usuario
+    database: '4bits'               // Nombre de la base de datos
 });
 
 // Se conecta a la base de datos, si hay un error detiene la ejecución del programa
 db.connect((err) => {
     if(err) throw err;
-});
-
-// Querry de prueba, imprime el resultado como los regresa mysql
-db.query('SELECT * FROM profesor', (err, res) => {
-    if(err) throw err;
-    console.log(res);
 });
 
 // Busca interfaces de red e imprime la dirección ip local
@@ -40,11 +37,32 @@ networkInterfaces()['Wi-Fi'].forEach((netInt) => {
 
 // Maneja la ruta base en GET, devuelve el html de la página principal 
 app.get('/', (req, res) => {
-    readFile('./index.html', 'utf-8', (err, html) => {
-        if(err) {
-            res.status(500).send('Server error');
-        }
-        res.send(html);
+    console.log(req.query)
+    if(!(req.query))
+    {
+        readFile('./index.html', 'utf-8', (err, html) => {
+            if(err) {
+                res.status(500).send('Server error');
+            }
+            res.send(html);
+    });
+    }
+    else 
+    {
+        db.query(`SELECT estado, luz FROM ocupado WHERE salon_id = ${req.query.cr} ORDER BY fecha DESC, hora DESC LIMIT 1;`, (err, result) => {
+            if(err) throw err;
+            console.log(result);
+            res.send(result);
+        });
+    }
+});
+
+app.get('/data', (req, res) => {
+    db.query(`SELECT * FROM ocupado`, (err, result) => {
+        if(err)
+            res.send('NULL');
+        else 
+            res.send(result);
     });
 });
 
@@ -60,9 +78,15 @@ app.post('/api', (req, res) => {
     console.log(req.body);
     // Cuando sepamos cual va a ser el formato con los datos que nos va a mandar el nodeMCU
     // los pasamos al INSERT INTO de la tabla de nuestra base de datos correspondiente
-    /*
-    let post = { id: 5, nombres: 'Juan', aPaterno: 'López', aMaterno: 'Castro', fNacimiento: '1999-01-01'};
-    db.query('INSERT INTO profesor SET ?', post, (err, result) => {
+    
+    let id_salon = req.body.salon_id;
+    let ocupado = req.body.estado;
+    let time = new Date(Date.now()).toISOString();
+    let date = time.split('T')[0];
+    let hour = time.split('T')[1].split('.')[0];
+    let querr = `INSERT INTO ocupado (salon_id, estado, fecha, hora, luz) VALUES (${id_salon},${ocupado},'${date}','${hour}', ${req.body.luz})`;
+    console.log(querr);
+    db.query(querr, (err, result) => {
         if(err) {
             console.log(err);
             //Hacer cosas si no se puede añadir a la base de datos
@@ -70,7 +94,7 @@ app.post('/api', (req, res) => {
             console.log(result);
         }
     });
-    */
+    
    // Respuesta del servidor para el NodeMCU
     res.send('Funcio');
 });
